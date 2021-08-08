b576e2e72ec06f9da369d650e56410dc04b26955
sarveshhome <sarvesh.carlton@gmail.com>
2021-08-08 14:10:08 +0100

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import App from "./App";
+import { createStore, combineReducers } from 'redux';
+const store = createStore();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App />,
+
+  document.getElementById('root')
+);
